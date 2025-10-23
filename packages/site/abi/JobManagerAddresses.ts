da72a2b8fd0e67c7e97f38fd0927f2dab641e8f3
ego-errante <foolt757@gmail.com>
2025-10-23 16:49:43 +0100

    deploy to sepolia

--- packages/site/abi/JobManagerAddresses.ts
+++ packages/site/abi/JobManagerAddresses.ts
@@ -4,6 +4,6 @@
   By commands: 'npx hardhat deploy' or 'npx hardhat node'
 */
 export const JobManagerAddresses = { 
-  "11155111": { address: "0x0000000000000000000000000000000000000000", chainId: 11155111, chainName: "sepolia" },
+  "11155111": { address: "0x3d921D887DDC947465975b345Cc91822E198F3B3", chainId: 11155111, chainName: "sepolia" },
   "31337": { address: "0xB2605A7326AEe6663Be4fbE03D3338aA6c1F2366", chainId: 31337, chainName: "hardhat" },
 };
